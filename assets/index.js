842875a4cf945986feb274955f248e73734e7bce
unknown <kov1606@gmail.com>
2023-03-26 01:01:20 +0300

    the first version of the gallery

---
+++ assets/index.js
@@ -0,0 +1,20 @@
+const blockFoto = document.getElementById('image');
+
+function pushNext() {
+    blockFoto.style.backgroundImage = "url('assets/images/1.jpg')";
+}
+
+function pushPrevious() {
+    blockFoto.style.backgroundImage = "url('assets/images/2.jpg')";
+}
+
+// const otherImage = document.getElementById('other-image');
+
+
+// const pushNext2 = (block) => {
+//     block.setAttribute = ('src', 'assets/images/cat2.jpg');
+//     otherImage.scr = "assets/images/cat3.jpg";
+//     console.log(otherImage);
+// };
+
+// function pushPrevious2() {}
