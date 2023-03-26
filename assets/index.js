6a235f7cfc482f6ac7b630c99b09a1d74e687f12
unknown <kov1606@gmail.com>
2023-03-26 13:28:42 +0300

    the second version of the gallery

--- assets/index.js
+++ assets/index.js
@@ -7,14 +7,3 @@ function pushNext() {
 function pushPrevious() {
     blockFoto.style.backgroundImage = "url('assets/images/2.jpg')";
 }
-
-// const otherImage = document.getElementById('other-image');
-
-
-// const pushNext2 = (block) => {
-//     block.setAttribute = ('src', 'assets/images/cat2.jpg');
-//     otherImage.scr = "assets/images/cat3.jpg";
-//     console.log(otherImage);
-// };
-
-// function pushPrevious2() {}
